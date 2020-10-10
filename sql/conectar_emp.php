787dad75d344588e0439e95d9da9b6f451e97b8c
Luis Alfredo SV <44524404+luisalfredosv@users.noreply.github.com>
2020-10-10 06:33:52 -0400

    Update conectar_emp.php

--- sql/conectar_emp.php
+++ sql/conectar_emp.php
@@ -1,6 +1,6 @@
 <?php
 //include("../conect.php");
-    $link = mysql_connect("localhost","root","kilo*sargento56")
+    $link = mysql_connect("localhost","user","password") 
         or die ("No me pude conectar");
     mysql_select_db ("empleados")
         or die ("No se puede seleccionar la base de datos");
